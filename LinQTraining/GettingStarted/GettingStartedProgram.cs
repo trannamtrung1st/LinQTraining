596f71d02f1dcce0b5103a370f163c94898b7e11
trannamtrung1st <trannamtrung1st@gmail.com>
2023-01-17 14:06:11 +0700

    Refactor

--- LinQTraining/GettingStarted/GettingStartedProgram.cs
+++ LinQTraining/GettingStarted/GettingStartedProgram.cs
@@ -14,10 +14,11 @@
 
             productList.AddRange(new[]
             {
-                new Product { Id = "apple", Name = "apple", CategoryId = "food" },
-                new Product { Id = "banana", Name = "banana", CategoryId = "food" },
-                new Product { Id = "orange", Name = "orange", CategoryId = "food" },
-                new Product { Id = "grape", Name = "grape", CategoryId = "food" },
+                new Product { Id = "apple", Name = "apple", CategoryId = "fruit" },
+                new Product { Id = "banana", Name = "banana", CategoryId = "fruit" },
+                new Product { Id = "orange", Name = "orange", CategoryId = "fruit" },
+                new Product { Id = "grape", Name = "grape", CategoryId = "fruit" },
+                new Product { Id = "mango", Name = "mango", CategoryId = "fruit" },
                 new Product { Id = "television", Name = "television", CategoryId = "electronic" },
                 new Product { Id = "laptop", Name = "laptop", CategoryId = "electronic" },
                 new Product { Id = "keyboard", Name = "keyboard", CategoryId = "electronic" },
@@ -30,7 +31,7 @@
 
             categoryList.AddRange(new[]
             {
-                new ProductCategory { Id = "food", Name = "food" },
+                new ProductCategory { Id = "fruit", Name = "fruit" },
                 new ProductCategory { Id = "electronic", Name = "electronic" },
                 new ProductCategory { Id = "household", Name = "household" }
             });
@@ -53,22 +54,32 @@
             GetAllProductsWithCategory();
 
             GetProductById();
+
+            GetAllProductsHavingNameStartWithA();
+
+            GetAllProductsHavingCategoryNameContainsE();
+
+            GetAllProductsGroupedByCategory();
+
+            GetProductOrderByCategoryNameDescAndProductNameAsc();
+
+            GetCountOfProductsGroupedByCategoryAndTheCategoryName();
         }
 
         private static void GetAllProductsAndCategories()
         {
-            Console.WriteLine("\n=== SelectAllProductsAndCategories ===");
+            Console.WriteLine("\n=== GetAllProductsAndCategories ===");
 
             var products = Products.ToList();
             var categories = Categories.ToList();
             PrintList(products, i => i.ToString()); PrintList(categories, i => i.ToString());
 
-            Console.WriteLine("=== SelectAllProductsAndCategories ===\n");
+            Console.WriteLine("=== GetAllProductsAndCategories ===\n");
         }
 
         private static void GetAllProductsWithCategory()
         {
-            Console.WriteLine("\n=== SelectAllProductsWithCategory ===");
+            Console.WriteLine("\n=== GetAllProductsWithCategory ===");
 
             var productsWithCategory = from product in Products
                                        join category in Categories on product.CategoryId equals category.Id
@@ -82,12 +93,12 @@
 
             PrintList(productsWithCategory, i => $"{i} | {i.Category}");
 
-            Console.WriteLine("=== SelectAllProductsWithCategory ===\n");
+            Console.WriteLine("=== GetAllProductsWithCategory ===\n");
         }
 
         private static void GetProductById()
         {
-            Console.WriteLine("\n=== SelectProductById ===");
+            Console.WriteLine("\n=== GetProductById ===");
 
             var productById = (from product in Products
                                where product.Id == "banana"
@@ -95,7 +106,91 @@
 
             Console.WriteLine(productById);
 
-            Console.WriteLine("=== SelectProductById ===\n");
+            Console.WriteLine("=== GetProductById ===\n");
+        }
+
+        private static void GetAllProductsHavingNameStartWithA()
+        {
+            Console.WriteLine("\n=== GetAllProductsHavingNameStartWithA ===");
+
+            var products = from product in Products
+                           where product.Name.StartsWith("A", StringComparison.OrdinalIgnoreCase)
+                           select product;
+
+            PrintList(products, i => i.ToString());
+
+            Console.WriteLine("=== GetAllProductsHavingNameStartWithA ===\n");
+        }
+
+        private static void GetAllProductsHavingCategoryNameContainsE()
+        {
+            Console.WriteLine("\n=== GetAllProductsHavingCategoryNameContainsE ===");
+
+            var products = from product in Products
+                           join category in Categories on product.CategoryId equals category.Id
+                           where category.Name.Contains("E", StringComparison.OrdinalIgnoreCase)
+                           select new
+                           {
+                               Product = product,
+                               Category = category
+                           };
+
+            PrintList(products, i => $"{i.Product} | {i.Category}");
+
+            Console.WriteLine("=== GetAllProductsHavingCategoryNameContainsE ===\n");
+        }
+
+        private static void GetAllProductsGroupedByCategory()
+        {
+            Console.WriteLine("\n=== GetAllProductsGroupedByCategory ===");
+
+            var groups = from product in Products
+                         group product by product.CategoryId;
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine($"Group: {group.Key}");
+
+                PrintList(group, i => i.ToString());
+            }
+
+            Console.WriteLine("=== GetAllProductsGroupedByCategory ===\n");
+        }
+
+        private static void GetProductOrderByCategoryNameDescAndProductNameAsc()
+        {
+            Console.WriteLine("\n=== GetProductOrderByCategoryNameDescAndProductNameAsc ===");
+
+            var products = from product in Products
+                           join category in Categories on product.CategoryId equals category.Id
+                           orderby category.Name descending, product.Name ascending
+                           select new
+                           {
+                               Product = product,
+                               Category = category
+                           };
+
+            PrintList(products, i => $"{i.Product} | {i.Category}");
+
+            Console.WriteLine("=== GetProductOrderByCategoryNameDescAndProductNameAsc ===\n");
+        }
+
+        private static void GetCountOfProductsGroupedByCategoryAndTheCategoryName()
+        {
+            Console.WriteLine("\n=== GetCountOfProductsGroupedByCategoryAndTheCategoryName ===");
+
+            var products = from product in Products
+                           group product by product.CategoryId into groups
+                           join category in Categories on groups.Key equals category.Id
+                           select new
+                           {
+                               Category = category,
+                               Count = groups.Count()
+                           };
+
+            PrintList(products, i => $"{i.Category} | Product count: {i.Count}");
+
+            Console.WriteLine("=== GetCountOfProductsGroupedByCategoryAndTheCategoryName ===\n");
         }
     }
 
