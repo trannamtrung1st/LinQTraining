27a273b10e1fcbd9baee8730698d3b13127a4cf2
trannamtrung1st <trannamtrung1st@gmail.com>
2023-01-25 15:10:56 +0700

    Add concat

--- LinQTraining/Operators/ConcatenationOperations.cs
+++ LinQTraining/Operators/ConcatenationOperations.cs
@@ -1,31 +1,15 @@
 ﻿namespace LinQTraining.Operators
 {
-    public static class ConvertingDataTypes
+    public static class ConcatenationOperations
     {
         public static void Run()
         {
-            try
-            {
-                IEnumerable<object> objects = new object[] { 1, 2, "Hello", new object() };
+            int[] a = new[] { 1, 2, 3, 4 };
+            int[] b = new[] { 4, 3, 2, 1 };
 
-                IQueryable<object> iqueryable = objects.AsQueryable();
+            var join = string.Join(',', a.Concat(b));
 
-                IEnumerable<object> enumerable = iqueryable.AsEnumerable();
-
-                IEnumerable<string> cast = from string s in objects
-                                           select s;
-
-                cast = objects.Cast<string>();
-
-                IEnumerable<string> ofType = from s in objects.OfType<string>()
-                                             select s;
-
-                object[] array = objects.ToArray();
-                List<object> list = objects.ToList();
-                Dictionary<Type, object> dictionary = objects.ToDictionary(e => e.GetType());
-                ILookup<Type, object> lookup = objects.ToLookup(e => e.GetType());
-            }
-            catch { }
+            Console.WriteLine(join);
         }
     }
 }
