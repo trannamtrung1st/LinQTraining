a15d6dc3f1c591e6211815d54c5f126faaeaea79
trannamtrung1st <trannamtrung1st@gmail.com>
2023-01-25 15:20:42 +0700

    Add ops

---
+++ LinQTraining/Operators/AggregationOperations.cs
@@ -0,0 +1,25 @@
+﻿namespace LinQTraining.Operators
+{
+    public static class AggregationOperations
+    {
+        public static void Run()
+        {
+            int[] numbers = new[] { 1, 2, 3, 4 };
+
+            string aggregate = numbers.Aggregate(string.Empty, (result, current) => result + current);
+
+            double avg = numbers.Average();
+            avg = numbers.Average(e => e * 2);
+
+            int count = numbers.Count();
+
+            int max = numbers.Max();
+            int maxBy = numbers.MaxBy(e => e % 2);
+            int min = numbers.Min();
+            int minBy = numbers.MinBy(e => e % 2);
+
+            int sum = numbers.Sum();
+            sum = numbers.Sum(e => e * 2);
+        }
+    }
+}
