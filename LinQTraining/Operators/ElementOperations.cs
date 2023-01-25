dbb1df80304d76bd3267b8adfaea7fb9761aef4f
trannamtrung1st <trannamtrung1st@gmail.com>
2023-01-25 15:08:15 +0700

    Add ops

--- LinQTraining/Operators/ElementOperations.cs
+++ LinQTraining/Operators/ElementOperations.cs
@@ -4,24 +4,28 @@
     {
         public static void Run()
         {
-            IEnumerable<object> objects = new object[] { 1, 2, "Hello", new object() };
+            try
+            {
+                IEnumerable<object> objects = new object[] { 1, 2, "Hello", new object() };
 
-            IQueryable<object> iqueryable = objects.AsQueryable();
+                IQueryable<object> iqueryable = objects.AsQueryable();
 
-            IEnumerable<object> enumerable = iqueryable.AsEnumerable();
+                IEnumerable<object> enumerable = iqueryable.AsEnumerable();
 
-            IEnumerable<string> cast = from string s in objects
-                                       select s;
+                IEnumerable<string> cast = from string s in objects
+                                           select s;
 
-            cast = objects.Cast<string>();
+                cast = objects.Cast<string>();
 
-            IEnumerable<string> ofType = from s in objects.OfType<string>()
-                                         select s;
+                IEnumerable<string> ofType = from s in objects.OfType<string>()
+                                             select s;
 
-            object[] array = objects.ToArray();
-            List<object> list = objects.ToList();
-            Dictionary<Type, object> dictionary = objects.ToDictionary(e => e.GetType());
-            ILookup<Type, object> lookup = objects.ToLookup(e => e.GetType());
+                object[] array = objects.ToArray();
+                List<object> list = objects.ToList();
+                Dictionary<Type, object> dictionary = objects.ToDictionary(e => e.GetType());
+                ILookup<Type, object> lookup = objects.ToLookup(e => e.GetType());
+            }
+            catch { }
         }
     }
 }
